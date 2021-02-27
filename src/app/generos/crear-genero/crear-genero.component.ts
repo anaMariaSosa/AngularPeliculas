5c23f34d53c2244f6afbca5fc47961e5ea003055
ana <anamaria.sosaberlanga@gmail.com>
2021-02-27 10:14:51 +0100

    form,input y validators reactivos

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { primeraLetraMayuscula } from 'src/app/utilidades/validadores/primeraLetra';
 
 @Component({
   selector: 'app-crear-genero',
@@ -7,14 +9,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./crear-genero.component.css']
 })
 export class CrearGeneroComponent implements OnInit {
+  form!: FormGroup;  
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form=this.formBuilder.group({
+      nombre:['',{validators:[Validators.required, Validators.minLength(2),primeraLetraMayuscula()]}]
+    });
   }
 
   saveGender():void{
     //llamar a servicio de guardar
     this.router.navigate(['/generos']);
+  }
+
+  obtainNameError(){
+    var field = this.form.get('nombre');
+    if(field != null && field.hasError('required'))
+    {
+        return 'Mandatory field. Please complete before save';
+    }
+    if(field != null && field.hasError('minlength'))
+    {
+        return 'Min Length allowed is 2 chars. Please review it before save';
+    }
+    if(field != null && field.hasError('primeraLetraMayuscula'))
+    {
+        return field.getError('primeraLetraMayuscula').mensaje;
+    }
+    return '';
   }
 }
---
+++ src/app/utilidades/validadores/primeraLetra.ts
@@ -0,0 +1,20 @@
+import { AbstractControl, ValidatorFn } from "@angular/forms";
+
+export function primeraLetraMayuscula(): ValidatorFn {
+    return (control: AbstractControl) => {
+        const valor = <string>control.value;
+        if (!valor) return null;
+        if (valor.length === 0) return null;
+
+        const primeraletra = valor[0];
+        if (primeraletra !== primeraletra.toUpperCase()) {
+            return {
+                primeraLetraMayuscula: {
+                    mensaje: 'First char shall be in upper case. Please review it before save'
+                }
+            };
+        }
+
+        return null;
+    } 
+}
